4f3e5a6b849e0e0afefc2517f55b1bd6cd1cda00
Ridvan Okovic <okovic.ridvan@gmail.com>
2022-07-13 11:47:31 +0200

    Updated main class

--- src/Junit/Main.java
+++ src/Junit/Main.java
@@ -1,4 +1,5 @@
 package Junit;
 
 public class Main {
+    
 }
--- src/exceptions/Main.java
+++ src/exceptions/Main.java
@@ -48,6 +48,7 @@ public class Main {
     public static void testMultipleExceptions() {
         int nums[] = new int[10];
         Scanner input = new Scanner(System.in);
+        System.out.println("Before try-catch block");
         System.out.println("Enter elements of an array");
         int num;
         for (int i = 0; i < nums.length; i++) {
@@ -56,10 +57,11 @@ public class Main {
         }
 
         try {
+            System.out.println("In try block");
             System.out.println("Enter index of an element you wish to access: ");
             int index = input.nextInt();
             if (index >= nums.length) {
-                throw new ArrayIndexOutOfBoundsException("Can not access that element index is too large");
+                throw new ArrayIndexOutOfBoundsException("First exception caught, can not access that element index is too large");
             }
             else {
                 System.out.println("Element at index: " + index + " is " + nums[index]);
@@ -67,9 +69,10 @@ public class Main {
 
             int divide;
             divide = nums[0]/nums[1];
-            throw new ArithmeticException("Can not divide a number with 0");
+            throw new ArithmeticException("Second exception caught, can not divide a number with 0");
         }
         catch (ArrayIndexOutOfBoundsException | ArithmeticException exception) {
+            System.out.println("In catch block");
             exception.printStackTrace();
         }
     }
